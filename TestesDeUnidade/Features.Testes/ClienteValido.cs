73e64b3a0fddf2c69ffad5ad1082c91036c71739
pedroMF1996 <pmfrp@hotmail.com>
2023-12-04 18:00:16 -0300

    Fixture Collections

---
+++ TestesDeUnidade/Features.Testes/ClienteValido.cs
@@ -0,0 +1,30 @@
+﻿using Features.Clientes;
+
+namespace Features.Testes
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteValido : IClassFixture<ClienteTestesFixture>
+    {
+        private readonly ClienteTestesFixture _clienteTestesFixture;
+
+        public ClienteValido(ClienteTestesFixture clienteTestesFixture)
+        {
+            _clienteTestesFixture = clienteTestesFixture;
+        }
+
+        [Fact(DisplayName = "Novo Cliente Valido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            //Arrange
+            var cliente = _clienteTestesFixture.GerarClienteValido();
+
+            //Act
+            var result = cliente.EhValido();
+
+            //Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
