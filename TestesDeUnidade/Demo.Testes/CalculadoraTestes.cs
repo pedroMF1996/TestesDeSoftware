a7b27eb15304e8ad0e3aee6499ae624fd210ea7e
pedroMF1996 <pmfrp@hotmail.com>
2023-12-04 16:09:02 -0300

    theory

--- TestesDeUnidade/Demo.Testes/CalculadoraTestes.cs
+++ TestesDeUnidade/Demo.Testes/CalculadoraTestes.cs
@@ -13,10 +13,30 @@ namespace Demo.Testes
         {
             //Arrange
             Calculadora calculadora = new();
+
             //Act
             var restultado = calculadora.Somar(2, 2);
+
             //Assert
             Assert.Equal(4, restultado);
+        }
+
+        [Theory]
+        [InlineData(2,2,4)]
+        [InlineData(1,2,3)]
+        [InlineData(2,7,9)]
+        [InlineData(2,5,7)]
+        [InlineData(2,3,5)]
+        public void Calculadora_Somar_RetornarValorSoma(double x, double y, double expectedResult)
+        {
+            //Arrange
+            Calculadora calculadora = new();
+
+            //Act
+            var restultado = calculadora.Somar(x, y);
+
+            //Assert
+            Assert.Equal(expectedResult, restultado);
         }
     }
 }
