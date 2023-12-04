4ac25eb29b02f6cf2a24db5546bc285903a8f815
pedroMF1996 <pmfrp@hotmail.com>
2023-12-04 15:54:16 -0300

    Primeiro teste

---
+++ TestesDeUnidade/Demo.Testes/CalculadoraTestes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Testes
+{
+    public class CalculadoraTestes
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarResuldadoSoma()
+        {
+            //Arrange
+            Calculadora calculadora = new();
+            //Act
+            var restultado = calculadora.Somar(2, 2);
+            //Assert
+            Assert.Equal(4, restultado);
+        }
+    }
+}
---
+++ TestesDeUnidade/Demo/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
