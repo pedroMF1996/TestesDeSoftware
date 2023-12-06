b20b82b656d50cb81db2d5c587b7c99d74207a09
pedroMF1996 <pmfrp@hotmail.com>
2023-12-06 13:10:49 -0300

    Skip

---
+++ TestesDeUnidade/Features.Testes/07 - Escapando Testes/TesteNaoPassandoMotivoEspecifico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Testes
+{
+    public class TesteNaoPassandoMotivoEspecifico
+    {
+        [Fact(DisplayName ="Novo Cliente 2.0", Skip = "Nova versao 2.0 quebrando")]
+        [Trait("Categoria", "Escapando os testes")]
+        public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
+        {
+            Assert.True(false);
+        }
+    }
+}
