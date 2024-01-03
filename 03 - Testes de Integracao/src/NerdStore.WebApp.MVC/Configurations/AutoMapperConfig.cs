69074f0dcef888b6f25ff375c10eb5aa0377a709
pmfrp1996 <pmfrp@hotmail.com>
2024-01-03 15:18:37 -0300

    SavePoint

---
+++ 03 - Testes de Integracao/src/NerdStore.WebApp.MVC/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace NerdStore.WebApp.MVC.Configurations
+{
+    public static class AutoMapperConfig
+    {
+        public static void AddAutoMapperConfig(this IServiceCollection services, Type[] types)
+        {
+            var mapperConfig = new MapperConfiguration(mc => {
+                                                                 types.ToList().ForEach(mc.AddProfile);
+                                                             });
+
+            services.AddSingleton(mapperConfig.CreateMapper());
+        }
+    }
+}
---
+++ 03 - Testes de Integracao/src/NerdStore.WebApp.MVC/Configurations/WebAppConfigurations.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Application.AutoMapper;
+using NerdStore.Catalogo.Data;
+using Nerdstore.Vendas.Data;
+using NerdStore.WebApp.MVC.Setup;
+using NerdStore.WebApp.MVC.Data;
+using Microsoft.OpenApi.Models;
+using AutoMapper;
+
+namespace NerdStore.WebApp.MVC.Configurations
+{
+    public static class WebAppConfigurations
+    {
+        public static void AddConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<CatalogoContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<VendasContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                    .AddDefaultUI()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddHttpContextAccessor();
+            services.AddSwaggerGen(c =>
+            {
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }}
+                };
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "desenvolvedor.io API",
+                    Description = "desenvolvedor.io  API",
+                    TermsOfService = null,
+                    Contact = new OpenApiContact { Name = "desenvolvedor.io", Email = "email@desenvolvedor.io", Url = new Uri("http://desenvolvedor.io") },
+                    License = new OpenApiLicense { Name = "MIT", Url = new Uri("http://desenvolvedor.io/licensa") }
+                });
+            });
+
+            services.AddAutoMapperConfig([typeof(DomainToViewModelMappingProfile), typeof(ViewModelToDomainMappingProfile)]);
+            
+            services.AddMediatR(cnfg => cnfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+            services.RegisterServices();
+        }
+
+        public static void UseConfigureServices(this IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Vitrine}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "desenvolvedor.io API v1.0");
+            });
+
+        }
+    }
+
+}
