6e47b974184054b2c93ba7eff0bbfcfbc6c42c91
pmfrp1996 <pmfrp@hotmail.com>
2024-01-05 11:28:41 -0300

    Usuario_RealizarLogin_DeveRealizarLoginComSucesso

--- TestesDeIntegracao/tests/NerStore.Integrations.Tests/UsuarioTests.cs
+++ TestesDeIntegracao/tests/NerStore.Integrations.Tests/UsuarioTests.cs
@@ -1,10 +1,13 @@
-﻿using FluentAssertions;
+﻿using Features.Testes;
+using FluentAssertions;
 using NerdStore.WebApp.MVC;
 using NerStore.Integrations.Tests.Config;
+using Xunit;
 
 namespace NerStore.Integrations.Tests
 {
     [Collection(nameof(IntegrationWebTestsFixtureCollection))]
+    [TestCaseOrderer("Features.Testes.PriorityOrderer", "Features.Testes")]
     public class UsuarioTests : IClassFixture<IntegrationTestsFixture<Program>>
     {
         private readonly IntegrationTestsFixture<Program> _fixture;
@@ -14,9 +17,9 @@ namespace NerStore.Integrations.Tests
             _fixture = fixture;
         }
 
-        [Fact(DisplayName = "Realizar Cadastro Com Sucesso")]
+        [Fact(DisplayName = "Realizar Cadastro Com Sucesso"), TestPriority(1)]
         [Trait("Categoria", "Integracao Web - Usuario")]
-        public async Task Usuario_RealizarCadastro_DeveRrealizarCadastroComSucesso()
+        public async Task Usuario_RealizarCadastro_DeveRealizarCadastroComSucesso()
         {
             // Arrange
             var urlRegister = "/Identity/Account/Register";
@@ -46,9 +49,9 @@ namespace NerStore.Integrations.Tests
             responseString.Should().Contain($"Hello {_fixture.UsuarioEmail}!");
         }
         
-        [Fact(DisplayName = "Nao Realizar Cadastro Com Sucesso De Usuario Com Senha Fraca")]
+        [Fact(DisplayName = "Nao Realizar Cadastro Com Sucesso De Usuario Com Senha Fraca"), TestPriority(2)]
         [Trait("Categoria", "Integracao Web - Usuario")]
-        public async Task UsuarioComSenhaFraca_RealizarCadastro_NaoDeveRrealizarCadastroComSucesso()
+        public async Task UsuarioComSenhaFraca_RealizarCadastro_NaoDeveRealizarCadastroComSucesso()
         {
             // Arrange
             var urlRegister = "/Identity/Account/Register";
@@ -75,11 +78,40 @@ namespace NerStore.Integrations.Tests
             // Assert
             var responseString = await postResponse.Content.ReadAsStringAsync();
             postResponse.EnsureSuccessStatusCode();
-            responseString.Should().NotContain($"Hello {_fixture.UsuarioEmail}!");
             responseString.Should().Contain("Passwords must have at least one non alphanumeric character.");
             responseString.Should().Contain("Passwords must have at least one lowercase (&#x27;a&#x27;-&#x27;z&#x27;).");
             responseString.Should().Contain("Passwords must have at least one uppercase (&#x27;A&#x27;-&#x27;Z&#x27;).");
         }
 
+        [Fact(DisplayName = "Deve Realizar Login Com Sucesso"), TestPriority(3)]
+        [Trait("Categoria", "Integracao Web - Usuario")]
+        public async Task Usuario_RealizarLogin_DeveRealizarLoginComSucesso()
+        {
+            // Arrange
+            var urlLogin = "/Identity/Account/Login";
+            var initialResponse = await _fixture.HttpClient.GetAsync(urlLogin);
+            initialResponse.EnsureSuccessStatusCode();
+            var antiForgeryToken = _fixture.ObiterAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
+
+            var formData = new Dictionary<string, string>()
+            {
+                { "Input.Email", _fixture.UsuarioEmail},
+                { "Input.Password", _fixture.UsuarioSenha},
+                { _fixture.AntiForgeryFieldName, antiForgeryToken}
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, urlLogin)
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+            // Act
+            var postResponse = await _fixture.HttpClient.SendAsync(postRequest);
+
+            // Assert
+            postResponse.EnsureSuccessStatusCode();
+            var postResponseString = await postResponse.Content.ReadAsStringAsync();
+            postResponseString.Should().Contain($"Hello {_fixture.UsuarioEmail}!");
+        }
     }
 }
