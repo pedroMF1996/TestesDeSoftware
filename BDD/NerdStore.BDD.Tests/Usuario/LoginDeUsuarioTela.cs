18d1a9b1facc03bece45debac50c683690d05985
pmfrp1996 <pmfrp@hotmail.com>
2024-01-10 11:40:06 -0300

    Usuario Login Step Concluido

---
+++ BDD/NerdStore.BDD.Tests/Usuario/LoginDeUsuarioTela.cs
@@ -0,0 +1,39 @@
+﻿using NerdStore.BDD.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.BDD.Tests.Usuario
+{
+    public class LoginDeUsuarioTela : BaseUsuarioTela
+    {
+        public LoginDeUsuarioTela(SeleniumHelper helper) : base(helper)
+        {
+        }
+
+        public void ClicarNoLogin()
+        {
+            Helper.ClicarNoLinkPorTexto("Login");
+        }
+
+        public void PreencherUsuarioESenha(UsuarioModel usuario)
+        {
+            Helper.PreencherTextBoxPorId("Input_Email", usuario.Email);
+            Helper.PreencherTextBoxPorId("Input_Password", usuario.Senha);
+        }
+
+        public bool ValidarPreenchimentoFormularioLogin(UsuarioModel usuario)
+        {
+            if (Helper.ObterValorTextBoxPorId("Input_Email") != usuario.Email) return false;
+            if (Helper.ObterValorTextBoxPorId("Input_Password") != usuario.Senha) return false;
+            return true;
+        }
+
+        public void ClicarNoBotaoDeLogin()
+        {
+            Helper.ClicarBotaoPorId("login-submit");
+        }
+    }
+}
--- BDD/NerdStore.BDD.Tests/Usuario/Usuario_LoginStepDefinitions.cs
+++ BDD/NerdStore.BDD.Tests/Usuario/Usuario_LoginStepDefinitions.cs
@@ -1,27 +1,58 @@
+using NerdStore.BDD.Tests.Config;
 using System;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace NerdStore.BDD.Tests.Usuario
 {
     [Binding]
-    public class Usuario_LoginStepDefinitions
+    [CollectionDefinition(nameof(AutomacaoFixureCollection))]
+    public class Usuario_LoginStepDefinitions : IClassFixture<AutomacaoWebFixture>
     {
+
+        private readonly AutomacaoWebFixture _fixture;
+        private readonly LoginDeUsuarioTela _loginDeUsuarioTela;
+        public Usuario_LoginStepDefinitions(AutomacaoWebFixture fixture)
+        {
+            _fixture = fixture;
+            _loginDeUsuarioTela = new LoginDeUsuarioTela(_fixture.BrowserHelper);
+        }
+
         [When(@"Ele clicar em login")]
         public void WhenEleClicarEmLogin()
         {
-            throw new PendingStepException();
+            //Arrange
+
+            //Act
+            _loginDeUsuarioTela.ClicarNoLogin();
+
+            //Assert
+            Assert.Equal(_fixture.Configuration.LoginUrl, _loginDeUsuarioTela.ObterUrl());
         }
 
         [When(@"Clicar no botao Login")]
         public void WhenClicarNoBotaoLogin()
         {
-            throw new PendingStepException();
+            //Arrange
+
+            //Act
+            _loginDeUsuarioTela.ClicarNoBotaoDeLogin();
+
+            //Assert
         }
 
         [When(@"Preencher os dados do formulario de login")]
         public void WhenPreencherOsDadosDoFormularioDeLogin(Table table)
         {
-            throw new PendingStepException();
+            //Arrange
+            var usuario = new UsuarioModel() { Email = "teste@teste.com", Senha = "Teste@123" };
+            _fixture.Usuario = usuario;
+
+            //Act
+            _loginDeUsuarioTela.PreencherUsuarioESenha(usuario);
+
+            //Assert
+            _loginDeUsuarioTela.ValidarPreenchimentoFormularioLogin(usuario);
         }
     }
 }
